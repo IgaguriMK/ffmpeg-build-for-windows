c77525618ccbb17ec1a7b1b4c70e1e7b0ca02e3c
Igaguri <igagurimk@gmail.com>
2018-07-05 15:36:10 +0900

    マウント確認ファイルを作成するように

--- run.sh
+++ run.sh
@@ -2,7 +2,11 @@
 
 set -ex
 
+echo running >> /mnt/artifacts/check-mount
+
 (cd /home/builder && su builder -c ./build.sh)
 cp /home/builder/bin/* /mnt/artifacts/
+
+rm /mnt/artifacts/check-mount
 
 tput bel
